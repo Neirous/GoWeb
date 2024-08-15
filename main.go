786b64541edb44eb760725a9239161dfb4f2449f
Neirous <381441075@qq.com>
2024-08-15 21:05:23 +0800

    add(render)

--- main.go
+++ main.go
@@ -2,16 +2,25 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 const portNumber = ":8080"
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, " This is home page")
+	renderTemplate(w, "home.page.tmpl")
 }
 func About(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, " This is about page")
+	renderTemplate(w, "about.page.tmpl")
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("err parsing template", err)
+	}
 }
 
 func main() {
