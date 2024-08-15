2c35d90d3d6d23b446f18cbe0b267d823ccffad5
Neirous <381441075@qq.com>
2024-08-15 23:31:55 +0800

    Building a simple template cache

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -2,6 +2,7 @@ package render
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"text/template"
 )
@@ -12,4 +13,43 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 	if err != nil {
 		fmt.Println("err parsing template", err)
 	}
+}
+
+var tc = make(map[string]*template.Template)
+
+func RenderTemplateTest(w http.ResponseWriter, t string) {
+	var tmpl *template.Template
+	var err error
+
+	_, inMap := tc[t]
+	if !inMap {
+		err = createTemplateCache(t)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		log.Println("using cached template")
+	}
+
+	tmpl = tc[t]
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func createTemplateCache(t string) error {
+	templates := []string{
+		fmt.Sprintf("../../templates/%s", t),
+		"../../templates/base.layout.tmpl",
+	}
+
+	tmpl, err := template.ParseFiles(templates...)
+	if err != nil {
+		return err
+	}
+
+	tc[t] = tmpl
+
+	return nil
 }
